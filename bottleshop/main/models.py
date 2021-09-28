ef8e83b221d848a81bc744fd3cf8da1335aace59
merinjo90 <merinjo90@gmail.com>
2021-09-28 01:52:34 -0400

    7.create Data base model in Django   7.1 Model created, name is Category.   7.2 After creating model ,we will go to settings.py file INSTALLED_APPS=[…'main'] define the apps   7.3 Register the django model[Category] in the admin panel       Import category model and register this model into the admin

--- bottleshop/main/models.py
+++ bottleshop/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title=models.CharField(max_length=100)
+    image=models.ImageField(upload_to="cat_imgs/")
+
+    def __str__(self):
+        return self.title
+
