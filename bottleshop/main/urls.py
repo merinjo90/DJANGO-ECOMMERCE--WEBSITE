83d4ae915381129b798d11d284d1084abb49010f
merinjo90 <merinjo90@gmail.com>
2021-09-30 07:07:02 -0400

    6.2 Create products list page (fetch products from the database)     1. Fetch all product from product mdel [view.py] then connect with main/urls.py        1.1 frontent template [product_list.html]        1.2 index.html setup path [url 'product-list'] all products defined        1.3 product_list.html give image and correspnding title     2.fetch category and brand from prduct model

--- bottleshop/main/urls.py
+++ bottleshop/main/urls.py
@@ -8,6 +8,7 @@ urlpatterns=[
     path('',views.home,name='home'),
     path('category-list', views.category_list, name='category-list'),
     path('brand-list', views.brand_list, name='brand-list'),
+    path('product-list', views.product_list, name='product-list'),
 
 ]
 
--- bottleshop/main/views.py
+++ bottleshop/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Category,Brand
+from .models import Category,Brand,Product
 
 # Create your views here.
 #Home
@@ -11,10 +11,22 @@ def category_list(request):
     data=Category.objects.all().order_by('-id')
     return render(request,'category_list.html',{'data':data})
 
-
 #Brand
 def brand_list(request):
     data=Brand.objects.all().order_by('-id')
     return render(request,'brand_list.html',{'data':data})
+
+#Product list
+def product_list(request):
+    data=Product.objects.all().order_by('-id')
+    cats=Product.objects.distinct().values('category__title','category__id')
+    brands=Product.objects.distinct().values('brand__title','brand__id')
+
+    return render(request,'product_list.html',
+        {
+            'data':data,
+            'cats':cats,
+            'brands':brands,
+        })
 
 
